eb245cb2af8916dbdfa9228e50a8d5bf6b000122
Kishore1K <reddykishore527@gmail.com>
2023-10-13 15:20:05 +0530

    HttpSession HttpContext

--- src/main/java/com/servlet/javalib/SimpleServlet.java
+++ src/main/java/com/servlet/javalib/SimpleServlet.java
@@ -3,11 +3,13 @@ package com.servlet.javalib;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SimpleServlet
@@ -22,9 +24,24 @@ public class SimpleServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello From Get Method");
+		response.setContentType("text/html");
 		PrintWriter writer = response.getWriter();
+		String name=  request.getParameter("uname");
 		writer.println("<h1>Hello World From Servlet </h1>");
-		response.getWriter().println("<h2>This is h2 </h2>");
+		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
+		
+		if(name != "" || !name.isEmpty()) {
+			session.setAttribute("savedName", name);
+			context.setAttribute("savedName",name);
+		}
+
+		writer.println("Hello "+name);
+		writer.println("Session "+(String)session.getAttribute("savedName"));
+		writer.println("Context "+(String)context.getAttribute("savedName"));
+
+
+		
 	}
 
 }
