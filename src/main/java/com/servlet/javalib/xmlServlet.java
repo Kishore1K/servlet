71294bb69c59f79ebbda972cedff78936260a597
Kishore1K <reddykishore527@gmail.com>
2023-10-13 13:55:46 +0530

    doGet & doPost methods

--- src/main/java/com/servlet/javalib/xmlServlet.java
+++ src/main/java/com/servlet/javalib/xmlServlet.java
@@ -1,6 +1,7 @@
 package com.servlet.javalib;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +15,27 @@ public class xmlServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		resp.getWriter().println("<h1>This is From /XML Servlet </h1>");
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<h1>This is From get method </h1>");
+		String uname = req.getParameter("uname"); 
+		String email = req.getParameter("email");
+		writer.println("<h1>This is From /XMLServlet 1 </h1>");
+		writer.println("Hello "+uname+" Id is "+email);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<h1>This is From Post method </h1>");
+		String uname = req.getParameter("uname"); 
+		String email = req.getParameter("email");
+		writer.println("<h1>This is From /XMLServlet 1 </h1>");
+		writer.println("Hello "+uname+" Id is "+email);
+		
+		
 	}
 
 }
